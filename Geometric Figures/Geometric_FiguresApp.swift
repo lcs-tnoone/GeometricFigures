f1bcc9206dae27c3b93fc0268bd04c003edb34e9
Thomas Noone <tnoone@lcs.on.ca>
2024-10-28 12:25:18 -0400

    Initial Commit

---
+++ Geometric Figures/Geometric_FiguresApp.swift
@@ -0,0 +1,17 @@
+//
+//  Geometric_FiguresApp.swift
+//  Geometric Figures
+//
+//  Created by Thomas Noone on 2024-10-28.
+//
+
+import SwiftUI
+
+@main
+struct Geometric_FiguresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
