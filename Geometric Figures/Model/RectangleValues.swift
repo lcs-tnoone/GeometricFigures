89ebf8257abcb2409254f586fa58b1d49ddbcd36
Thomas Noone <tnoone@lcs.on.ca>
2024-10-28 20:37:04 -0400

    Added Rectangular Prism Structure and made a basic view to control Height

---
+++ Geometric Figures/Model/RectangleValues.swift
@@ -0,0 +1,8 @@
+//
+//  RectangleValues.swift
+//  Geometric Figures
+//
+//  Created by Thomas Noone on 2024-10-28.
+//
+
+import Foundation
---
+++ Geometric Figures/Views/RectangularPrism.swift
@@ -0,0 +1,18 @@
+//
+//  RectangularPrism.swift
+//  Geometric Figures
+//
+//  Created by Thomas Noone on 2024-10-28.
+//
+
+import SwiftUI
+
+struct RectangularPrism: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RectangularPrism()
+}
